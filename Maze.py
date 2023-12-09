e4a7f83c5d2591b233c6e1a48675ee901306dd42
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2023-12-09 19:08:04 +0100

    Update Maze.py

--- Maze.py
+++ Maze.py
@@ -10,12 +10,12 @@ colorama_init()
 class Maze:
     class Agent:
         def __init__(self, label: str, coords: tuple) -> None:
-            self.label = label
-            self.coords = coords
+            self.label: str = label
+            self.coords: tuple = coords
             
     def __init__(self, width: int, height: int, agent_label: str = "@", enable_colors: bool = True, end_symbol: str = f"{Fore.RED}E{Style.RESET_ALL}", start_symbol: str = "S") -> None:
         self.maze, self.start, self.end, self.end_symbol, self.start_symbol = self.ttg(self.generate_maze, width=width, height=height, end_symbol=end_symbol, start_symbol=start_symbol)
-        self.colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.YELLOW]
+        self.colors: str = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.YELLOW]
         self.agent: self.Agent = self.Agent(agent_label if not enable_colors else f"{random.choice(self.colors)}{agent_label}{Style.RESET_ALL}", self.start)
         self.move(self.start)
         
@@ -30,7 +30,7 @@ class Maze:
 
         is_valid = lambda x, y: 0 <= x < width and 0 <= y < height and maze[y][x] == '#'
 
-        def carve_path(x, y):
+        def carve_path(x: int, y: int) -> None:
             directions: list = [(2, 0), (-2, 0), (0, 2), (0, -2)]
             random.shuffle(directions)
 
@@ -78,7 +78,7 @@ class Maze:
         return [(x, y) for x, y in ((coords[0], coords[1] - 1), (coords[0], coords[1] + 1), (coords[0] + 1, coords[1]), (coords[0] - 1, coords[1])) if self.maze[y][x] == ' ']
     
     def move(self, new_coords: tuple) -> None:
-        self.agent.coords = new_coords
+        self.agent.coords: tuple = new_coords
         self.maze[new_coords[1]][new_coords[0]] = self.agent.label
         self.log()
         sys.stdout.flush()
